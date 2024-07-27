02cef95467500288b2d8234e117c2722f21801e8
Allan <amramogo@gmail.com>
2024-07-27 09:56:38 +0300

    Force and change types of data being saved and insert it into database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,16 +1,46 @@
 'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+/**
+ * Creates a new invoice based on the provided form data.
+ *
+ * @param formData - The form data containing the invoice details.
+ * @returns A Promise that resolves when the invoice is created.
+ */
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerID: formData.get('customerId'),
+    const {customerId, amount, status} = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
+    });
+    const amountInCents = amount * 100;
+    const date = new  Date().toISOString().split('T')[0];
 
     const rawFormData2 = Object.fromEntries(formData.entries());
     
     // View in console
-    console.log('Raw form data: ', rawFormData);
+    console.log('customerId: ', customerId);
+    console.log('amount: ', amount);
+    console.log('status: ', status);
+    console.log('amountInCents: ', amountInCents);
+    console.log('date: ', date);
+    console.log('Raw form data: ', formData);
+    console.log('Type of customerId: ', typeof customerId);
+    console.log('Type of amount: ', typeof amount);
+    console.log('Type of status: ', typeof status);
     console.log('Raw form data 2: ', rawFormData2);
-    
+    await sql `
+        INSERT INTO invoices(customer_id, amount, status, date)
+        VALUES(${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 }
