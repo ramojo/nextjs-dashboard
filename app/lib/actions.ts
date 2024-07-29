36a20deea2a6a4eda038c9fa98b6bb56f10f5d01
Allan <amramogo@gmail.com>
2024-07-29 19:25:14 +0300

    Server side form validation for creating an invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,12 +6,26 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Amount must be greater than $0' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select a status',
+    }),
     date: z.string(),
 });
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
 /**
  * Creates a new invoice based on the provided form data.
  *
@@ -21,27 +35,38 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-    const {customerId, amount, status} = CreateInvoice.parse({
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
+    console.log('validatedFields: ', validatedFields);
+    // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = validatedFields.data.amount * 100;
+    console.log('amountInCents: ', amountInCents);
     const date = new  Date().toISOString().split('T')[0];
 
     const rawFormData2 = Object.fromEntries(formData.entries());
     
     // View in console
-    console.log('customerId: ', customerId);
-    console.log('amount: ', amount);
-    console.log('status: ', status);
+    console.log('customerId: ', validatedFields.data.customerId);
+    console.log('amount: ', validatedFields.data.amount);
+    console.log('status: ', validatedFields.data.status);
     console.log('amountInCents: ', amountInCents);
     console.log('date: ', date);
     console.log('Raw form data: ', formData);
-    console.log('Type of customerId: ', typeof customerId);
-    console.log('Type of amount: ', typeof amount);
-    console.log('Type of status: ', typeof status);
+    console.log('Type of customerId: ', typeof validatedFields.data.customerId);
+    console.log('Type of amount: ', typeof validatedFields.data.amount);
+    console.log('Type of status: ', typeof validatedFields.data.status);
     console.log('Raw form data 2: ', rawFormData2);
     try {
     await sql `
