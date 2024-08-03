6dd43c49f5bb40afccb069436bebb93b4056a96a
Allan <amramogo@gmail.com>
2024-08-03 20:27:30 +0300

    Fixed customers page pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,11 @@
-import Pagination from '@/app/ui/invoices/pagination';
+import Pagination from '@/app/ui/customers/pagination';
 import Search from '@/app/ui/search';
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import { TableRowSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import { fetchCustomersPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 import { FormattedCustomersTable } from '@/app/lib/definitions';
 
@@ -24,18 +24,18 @@ export default async function Page({
   const query = searchParams?.searchString || '';
   const currentPage = Number(searchParams?.page || 1);
 
-  const totalPages = await fetchInvoicesPages(query);
+  const totalPages = await fetchCustomersPages(query);
   const customers = await fetchFilteredCustomers(query);
 
   return (
     <div className="w-full">
       
-      {/* <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}> */}
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
         <CustomersTable customers={customers} />
-      {/* </Suspense> */}
-      {/* <div className="mt-5 flex w-full justify-center"> */}
-        {/* <Pagination totalPages={totalPages} /> */}
-      {/* </div> */}
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -220,3 +220,21 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+export async function fetchCustomersPages(query: string) {
+  try {
+    const count = await sql`SELECT COUNT(*)
+    FROM customers
+    WHERE
+      customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
+
+  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of invoices.');
+  }
+}
