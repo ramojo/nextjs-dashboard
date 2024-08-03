59747f5f7c6bbd1d783edf4f87c5661f7316391f
Allan <amramogo@gmail.com>
2024-08-03 20:19:27 +0300

    Fixed search on customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,26 +1,41 @@
-import { fetchFilteredCustomers } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
 import CustomersTable from '@/app/ui/customers/table';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
-
+ 
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
-    query?: string;
+    searchString?: string;
     page?: string;
   };
 }) {
-  const query = searchParams?.query || '';
+  const query = searchParams?.searchString || '';
+  const currentPage = Number(searchParams?.page || 1);
 
+  const totalPages = await fetchInvoicesPages(query);
   const customers = await fetchFilteredCustomers(query);
 
   return (
-    <main>
-      <CustomersTable customers={customers} />
-    </main>
+    <div className="w-full">
+      
+      {/* <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}> */}
+        <CustomersTable customers={customers} />
+      {/* </Suspense> */}
+      {/* <div className="mt-5 flex w-full justify-center"> */}
+        {/* <Pagination totalPages={totalPages} /> */}
+      {/* </div> */}
+    </div>
   );
 }
